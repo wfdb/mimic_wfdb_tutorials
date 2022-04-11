a85b70d1ae355e56d46e81553f8d5e3330e244dd
Peter H Charlton <peterhcharlton@gmail.com>
2022-04-11 16:59:43 +0100

    brief edit

--- _build/jupyter_execute/data-extraction.py
+++ _build/jupyter_execute/data-extraction.py
@@ -38,38 +38,23 @@ print("Selected record: {}".format(selected_record))
 
 
 # load data from this record
-no_seconds_to_load = 5
-record_data = wfdb.rdrecord(record_name=selected_record, sampfrom=0, sampto=125*no_seconds_to_load, pn_dir=database_name) 
-print("{} seconds of data loaded from: {}".format(no_seconds_to_load, selected_record))
+record_data = wfdb.rdrecord(record_name=selected_record, pn_dir=database_name) 
+print("Data loaded from record: {}".format(selected_record))
 
-
-# ## Plot signals in this record
-# Plot the signals contained in this record
 
 # In[5]:
 
 
-# plot the data loaded from this record
-title_text = "First record from " + database_name + " (record: " + selected_record + ")"
-wfdb.plot_wfdb(record=record_data, title=title_text, time_units='seconds') 
+# Look at class type of the object in which the data are stored:
+print("Data stored in class of type: {}".format(type(record_data)))
 
 
 # In[6]:
 
 
-record_contains_signals_log = 'RESP,' in record_data.sig_name and 'PLETH,' in record_data.sig_name
-if record_contains_signals_log:
-    print('This record contains the required signals')
-else:
-    print('This record doesn\'t contain the required signals')
-
-
-# In[7]:
-
-
-# plot just the signals of interest from this record
-title_text = "Selected signals from: " + selected_record
-wfdb.plot_items(signal=record_data.p_signal[:,0:2], title = title_text, ylabel = record_data.sig_name[0:2])
+# look at variables contained within the object
+from pprint import pprint
+pprint(vars(record_data))
 
 
 # In[ ]:
